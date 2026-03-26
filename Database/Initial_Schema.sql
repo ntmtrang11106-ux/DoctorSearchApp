8ed8722f3ba9f6a24958f77a6ed08e7056b983ea
vothinhan0106 <vothinhan01062006@gmail.com>
2026-03-26 21:08:02 +0700

    Update Initial_Schema.sql

--- Database/Initial_Schema.sql
+++ Database/Initial_Schema.sql
@@ -1,4 +1,13 @@
-//ĐỊNH NGHĨA CÁC BẢNG CỐT LỖI
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'CNPM')
+BEGIN
+    CREATE DATABASE CNPM;
+END
+GO
+
+USE CNPM;
+GO
+
+--ĐỊNH NGHĨA CÁC BẢNG CỐT LỖI
 -- 1. Bảng Users (Gốc của mọi tài khoản)
 CREATE TABLE Users (
     Id INT IDENTITY(1,1) PRIMARY KEY,
@@ -11,37 +20,37 @@ CREATE TABLE Users (
 -- 2. Bảng Patients (Thông tin chi tiết Bệnh nhân)
 CREATE TABLE Patients (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    user_Id INT NOT NULL, -- Nối với Users
+    UserId INT NOT NULL, -- Nối với Users
     full_name NVARCHAR(100) NOT NULL,
     dob DATE,
     address NVARCHAR(255),
-    CONSTRAINT FK_Patients_Users FOREIGN KEY (user_id) REFERENCES Users(id)
+    CONSTRAINT FK_Patients_Users FOREIGN KEY (UserId) REFERENCES Users(id)
 );
 
 -- 3. Bảng Doctors (Thông tin chi tiết Bác sĩ)
 CREATE TABLE Doctors (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    user_Id INT NOT NULL, -- Nối với Users
+    UserId INT NOT NULL, -- Nối với Users
     full_name NVARCHAR(100) NOT NULL,
     specialty NVARCHAR(100), -- Chuyên khoa
     experience_years INT,
     bio NVARCHAR(MAX),
-    CONSTRAINT FK_Doctors_Users FOREIGN KEY (user_id) REFERENCES Users(id)
+    CONSTRAINT FK_Doctors_Users FOREIGN KEY (UserId) REFERENCES Users(id)
 );
 
 -- 4. Bảng MedicalRecords (Hồ sơ bệnh án - Nối Bệnh nhân và Bác sĩ)
 CREATE TABLE MedicalRecords (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    patient_Id INT NOT NULL,
-    doctor_Id INT NOT NULL,
+    PatientId INT NOT NULL,
+    DoctorId INT NOT NULL,
     visit_date DATETIME DEFAULT GETDATE(),
     diagnosis NVARCHAR(MAX), -- Chẩn đoán
     treatment NVARCHAR(MAX), -- Hướng điều trị
-    CONSTRAINT FK_Records_Patients FOREIGN KEY (patient_id) REFERENCES Patients(id),
-    CONSTRAINT FK_Records_Doctors FOREIGN KEY (doctor_id) REFERENCES Doctors(id)
+    CONSTRAINT FK_Records_Patients FOREIGN KEY (PatientId) REFERENCES Patients(id),
+    CONSTRAINT FK_Records_Doctors FOREIGN KEY (DoctorId) REFERENCES Doctors(id)
 );
 
-//ĐỊNH NGHĨA CÁC BẢNG NGHIỆP VỤ
+--ĐỊNH NGHĨA CÁC BẢNG NGHIỆP VỤ
     -- 1. Bảng Chuyên khoa
 CREATE TABLE Specialties (
     Id INT IDENTITY(1,1) PRIMARY KEY, -- Chuẩn hóa Id 
@@ -64,7 +73,7 @@ CREATE TABLE TimeSlots (
     EndTime TIME NOT NULL,
     Status NVARCHAR(20) DEFAULT N'Trống',
     -- Lưu ý: Cần có bảng Doctors của Trang để tạo FK này
-    -- CONSTRAINT FK_TimeSlots_Doctors FOREIGN KEY (DoctorId) REFERENCES Doctors(Id)
+    CONSTRAINT FK_TimeSlots_Doctors FOREIGN KEY (DoctorId) REFERENCES Doctors(Id)
 );
 
 -- 4. Bảng Lịch hẹn
@@ -79,63 +88,62 @@ CREATE TABLE Appointments (
     CONSTRAINT FK_App_TimeSlot FOREIGN KEY (TimeSlotId) REFERENCES TimeSlots(Id)
 );
 
-//ĐỊNH NGHĨA CÁC BẢNG BỔ TRỢ
+--ĐỊNH NGHĨA CÁC BẢNG BỔ TRỢ
 -- 1. Đặc tả cho Reviews (Đánh giá)
 CREATE TABLE Reviews (
-    ReviewID INT PRIMARY KEY IDENTITY(1,1),
-    AppID INT NOT NULL UNIQUE,                   -- Liên kết 1-1 tới lần khám
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    AppointmentId INT NOT NULL UNIQUE,                   -- Liên kết 1-1 tới lần khám
     Rating INT CHECK (Rating >= 1 AND Rating <= 5), -- Số sao (1-5)
     Comment NVARCHAR(MAX),                       -- Nhận xét của bệnh nhân
     CreatedAt DATETIME DEFAULT GETDATE(),        -- Ngày tạo đánh giá
-    CONSTRAINT FK_Reviews_App FOREIGN KEY (AppID) REFERENCES Appointments(AppID)
+    CONSTRAINT FK_Reviews_App FOREIGN KEY (AppointmentId) REFERENCES Appointments(Id)
 );
 
 -- 2. Đặc tả cho Conversations (Hội thoại)
 CREATE TABLE Conversations (
-    ConvID INT PRIMARY KEY IDENTITY(1,1),
+    Id INT PRIMARY KEY IDENTITY(1,1),
     PatientID INT NOT NULL,                      -- FK trỏ về Patients
     DoctorID INT NOT NULL,                       -- FK trỏ về Doctors
     LastActive DATETIME DEFAULT GETDATE(),       -- Thời điểm tương tác cuối
-    CONSTRAINT FK_Conv_Patient FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
-    CONSTRAINT FK_Conv_Doctor FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID)
+    CONSTRAINT FK_Conv_Patient FOREIGN KEY (PatientID) REFERENCES Patients(Id),
+    CONSTRAINT FK_Conv_Doctor FOREIGN KEY (DoctorID) REFERENCES Doctors(Id)
 );
 
 -- 3. Đặc tả cho Messages (Tin nhắn)
 CREATE TABLE Messages (
-    MsgID INT PRIMARY KEY IDENTITY(1,1),
-    ConvID INT NOT NULL,                         -- Thuộc đoạn chat nào
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    ConversationId INT NOT NULL,                         -- Thuộc đoạn chat nào
     SenderID INT NOT NULL,                       -- ID của người gửi (Trỏ về Users)
     Content NVARCHAR(MAX) NOT NULL,               -- Nội dung tin nhắn
     SentAt DATETIME DEFAULT GETDATE(),           -- Thời điểm gửi tin
     IsRead BIT DEFAULT 0,                        -- 0: chưa đọc, 1: đã xem
-    CONSTRAINT FK_Msg_Conv FOREIGN KEY (ConvID) REFERENCES Conversations(ConvID),
-    CONSTRAINT FK_Msg_Sender FOREIGN KEY (SenderID) REFERENCES Users(UserID)
+    CONSTRAINT FK_Msg_Conv FOREIGN KEY (ConversationId) REFERENCES Conversations(Id),
+    CONSTRAINT FK_Msg_Sender FOREIGN KEY (SenderID) REFERENCES Users(Id)
 );
 
 -- 4. Đặc tả cho CallLogs (Nhật ký cuộc gọi)
 CREATE TABLE CallLogs (
-    CallID INT PRIMARY KEY IDENTITY(1,1),
+    Id INT PRIMARY KEY IDENTITY(1,1),
     CallerID INT NOT NULL,                       -- ID người gọi
     ReceiverID INT NOT NULL,                     -- ID người nhận
     StartTime DATETIME NOT NULL,                 -- Thời điểm bắt đầu
     EndTime DATETIME NOT NULL,                   -- Thời điểm kết thúc
     Duration INT DEFAULT 0,                      -- Thời lượng (giây)
     Status NVARCHAR(50) NOT NULL,                -- Missed, Completed, Declined
-    CONSTRAINT FK_Call_Caller FOREIGN KEY (CallerID) REFERENCES Users(UserID),
-    CONSTRAINT FK_Call_Receiver FOREIGN KEY (ReceiverID) REFERENCES Users(UserID)
+    CONSTRAINT FK_Call_Caller FOREIGN KEY (CallerID) REFERENCES Users(Id),
+    CONSTRAINT FK_Call_Receiver FOREIGN KEY (ReceiverID) REFERENCES Users(Id)
 );
 
 -- 5. Đặc tả cho Articles (Bài viết y khoa)
 -- Lưu ý: AdminID trỏ về Users.UserID
 CREATE TABLE Articles (
-    ArticleID INT PRIMARY KEY IDENTITY(1,1),
+    Id INT PRIMARY KEY IDENTITY(1,1),
     AdminID INT NOT NULL,                        -- FK trỏ về Users.UserID
     Title NVARCHAR(255) NOT NULL,                -- Tiêu đề bài viết
     Content NVARCHAR(MAX) NOT NULL,               -- Nội dung chi tiết
     Thumbnail VARCHAR(255),                      -- Ảnh đại diện (đường dẫn file)
     Views INT DEFAULT 0,                         -- Tổng lượt xem
     CreatedAt DATETIME DEFAULT GETDATE(),        -- Ngày xuất bản
-    CONSTRAINT FK_Articles_Admin FOREIGN KEY (AdminID) REFERENCES Users(UserID)
+    CONSTRAINT FK_Articles_Admin FOREIGN KEY (AdminID) REFERENCES Users(Id)
 );
-
-
+GO
