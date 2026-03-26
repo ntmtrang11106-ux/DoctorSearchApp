59d6b96b7c75ed10ebe96ed565c1fdb34266d220
iris on iries <133247922+Seer-2210@users.noreply.github.com>
2026-03-26 20:25:25 +0700

    Update Bussines.sql

--- Database/Bussines.sql
+++ Database/Bussines.sql
@@ -1,19 +1,19 @@
 -- 1. Bảng Chuyên khoa
 CREATE TABLE Specialties (
-    SpecId INT IDENTITY(1,1) PRIMARY KEY, -- Chuẩn hóa Id 
+    Id INT IDENTITY(1,1) PRIMARY KEY, -- Chuẩn hóa Id 
     Name NVARCHAR(100) NOT NULL,
     Description NVARCHAR(MAX)
 );
 
 -- 2. Bảng Khu vực
 CREATE TABLE Locations (
-    LocationId INT IDENTITY(1,1) PRIMARY KEY,
+    Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL
 );
 
 -- 3. Bảng Khung giờ (Bác sĩ tạo lịch)
 CREATE TABLE TimeSlots (
-    TimeId INT IDENTITY(1,1) PRIMARY KEY,
+    Id INT IDENTITY(1,1) PRIMARY KEY,
     DoctorId INT NOT NULL, -- Theo công thức TableNameId 
     Date DATE NOT NULL,
     StartTime TIME NOT NULL,
@@ -25,12 +25,12 @@ CREATE TABLE TimeSlots (
 
 -- 4. Bảng Lịch hẹn
 CREATE TABLE Appointments (
-    AppId INT IDENTITY(1,1) PRIMARY KEY,
+    Id INT IDENTITY(1,1) PRIMARY KEY,
     PatientId INT NOT NULL, -- FK trỏ về Patients.Id 
     TimeSlotId INT NOT NULL, -- FK trỏ về TimeSlots.Id 
     Symptoms NVARCHAR(500),
     Status NVARCHAR(20) DEFAULT N'Chờ duyệt',
     CreatedAt DATETIME DEFAULT GETDATE(),
-    CONSTRAINT FK_App_Patient FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
-    CONSTRAINT FK_App_TimeSlot FOREIGN KEY (TimeSlotId) REFERENCES TimeSlots(TimeId)
+    CONSTRAINT FK_App_Patient FOREIGN KEY (PatientId) REFERENCES Patients(Id),
+    CONSTRAINT FK_App_TimeSlot FOREIGN KEY (TimeSlotId) REFERENCES TimeSlots(Id)
 );
