577f1810ddd6543a130443eda00a575b9ab66b58
iris on iries <tientri2210@gmail.com>
2026-04-20 23:59:37 +0700

    save before choke

---
+++ DoctorSearchPBL3/UI_Tier/ucDoctorCertificate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI_Tier
+{
+    public partial class ucDoctorCertificate : UserControl
+    {
+        public ucDoctorCertificate()
+        {
+            InitializeComponent();
+        }
+    }
+}
